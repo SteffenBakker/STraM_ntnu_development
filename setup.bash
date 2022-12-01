d9afaaed8c2b23f322418f6648b0235c2b8198ea
Steffen Jaap Bakker <steffen.bakker@ntnu.no>
2022-12-01 15:44:48 +0100

    Removind old files

--- setup.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Author: Aksel Kristiansen Borgmo
-# General shell script for parallel execution of script follows here:
-
-module load Python
-module load gurobi
-pip install pandas
