a30730513602da87d9a417cbe2515ac83e245006
Steffen Jaap Bakker <steffen.bakker@ntnu.no>
2023-01-20 18:11:06 +0100

    Update read_file_pickle.py

--- read_file_pickle.py
+++ read_file_pickle.py
@@ -1,6 +1,6 @@
 import pickle
 
-with open(r'Data\\output_data_EV', 'rb') as output_file:
+with open(r'Data\output_data_EV', 'rb') as output_file:
     output_evp = pickle.load(output_file)
 
 print('finished')
