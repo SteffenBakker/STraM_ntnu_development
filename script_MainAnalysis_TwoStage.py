e9e0f8bdd727c920e8fd5c32a33e2c08c2e3bc36
Steffen Jaap Bakker <steffen.bakker@ntnu.no>
2023-02-11 15:59:14 +0100

    try all instances

--- script_MainAnalysis_TwoStage.py
+++ script_MainAnalysis_TwoStage.py
@@ -51,7 +51,7 @@ profiling = False
 distribution_on_cluster = False  #is the code to be run on the cluster using the distribution package?
 
 analysis_type = 'SP' #, 'EEV' , 'SP'         expected value probem, expectation of EVP, stochastic program
-sheet_name_scenarios = 'three_scenarios_new' #scenarios_base,three_scenarios_new, three_scenarios_with_maturity
+sheet_name_scenarios = 'scenarios_base' #scenarios_base,three_scenarios_new, three_scenarios_with_maturity
 time_periods = None  #[2022,2026,2030] or None for default up to 2050
 
 # risk parameters
@@ -126,7 +126,7 @@ def construct_and_solve_SP(base_data,
 
     # ------ CONSTRUCT MODEL ----------#
 
-    print("Constructing SP model...", flush=True)
+    print("Constructing SP model...")
 
     start = time.time()
     model_instance = TranspModel(data=base_data, risk_info=risk_info)
