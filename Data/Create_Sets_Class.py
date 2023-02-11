38ae4606255cff82782423e90866ec6f6f0807e4
Steffen Jaap Bakker <steffen.bakker@ntnu.no>
2023-02-11 12:25:25 +0100

    Update Create_Sets_Class.py

--- Data/Create_Sets_Class.py
+++ Data/Create_Sets_Class.py
@@ -837,7 +837,7 @@ class TransportSets():
         for s in self.S_SCENARIOS:
             for ss in self.S_SCENARIOS:
                 if (s != ss) and ((ss,s) not in self.SS_SCENARIOS_NONANT ):
-                self.SS_SCENARIOS_NONANT.append((s,ss))
+                    self.SS_SCENARIOS_NONANT.append((s,ss))
 
         self.T_TIME_FIRST_STAGE = [t for t in self.T_TIME_FIRST_STAGE_BASE if t in self.T_TIME_PERIODS]  
         self.T_TIME_SECOND_STAGE = [t for t in self.T_TIME_SECOND_STAGE_BASE if t in self.T_TIME_PERIODS]  
