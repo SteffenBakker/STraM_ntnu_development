e7721975f48e261f6020afd3c19ef0bf9f146dec
Steffen Jaap Bakker <steffen.bakker@ntnu.no>
2023-02-13 08:40:49 +0100

    test

--- script_MainAnalysis_TwoStage.py
+++ script_MainAnalysis_TwoStage.py
@@ -53,7 +53,7 @@ distribution_on_cluster = False  #is the code to be run on the cluster using the
 
 analysis_type = 'SP' #, 'EEV' , 'SP'         expected value probem, expectation of EVP, stochastic program
 wrm_strt = False  #use EEV as warm start for SP
-sheet_name_scenarios = 'scenarios_base' #scenarios_base,three_scenarios_new, three_scenarios_with_maturity
+sheet_name_scenarios = 'three_scenarios_new' #scenarios_base,three_scenarios_new, three_scenarios_with_maturity
 time_periods = None  #[2022,2026,2030] or None for default up to 2050
 
 # risk parameters
