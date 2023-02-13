f77fd739f3741be02505beaad2a3c657fcfafa14
Steffen Jaap Bakker <steffen.bakker@ntnu.no>
2023-02-13 09:32:22 +0100

    Update script_MainAnalysis_TwoStage.py

--- script_MainAnalysis_TwoStage.py
+++ script_MainAnalysis_TwoStage.py
@@ -51,7 +51,7 @@ profiling = False
 distribution_on_cluster = False  #is the code to be run on the cluster using the distribution package?
 
 analysis_type = 'SP' #, 'EEV' , 'SP'         expected value probem, expectation of EVP, stochastic program
-wrm_strt = True  #use EEV as warm start for SP
+wrm_strt = False  #use EEV as warm start for SP
 sheet_name_scenarios = 'scenarios_base' #scenarios_base,three_scenarios_new, three_scenarios_with_maturity
 time_periods = None  #[2022,2026,2030] or None for default up to 2050
 
