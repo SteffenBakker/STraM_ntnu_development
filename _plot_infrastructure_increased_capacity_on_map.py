8de30bc36a0afdc514fa60f2b95c637e489bc72b
Sander Boye Eikeland <sandee@stud.ntnu.no>
2024-02-03 17:10:35 +0100

    Make plot for investment in expansion (node and railways)

--- _plot_infrastructure_increased_capacity_on_map.py
+++ _plot_infrastructure_increased_capacity_on_map.py
@@ -20,7 +20,6 @@ from _plot_base_map import plot_base_map_start
 
 def process_epsilon_edges(epsilon_edge, sel_time_period, sel_scenario, cumulative):
     
-    print(epsilon_edge.head())
     
     epsilon_edge = epsilon_edge.copy()
     
@@ -32,7 +31,6 @@ def process_epsilon_edges(epsilon_edge, sel_time_period, sel_scenario, cumulativ
     else:
         epsilon_edge = epsilon_edge[epsilon_edge['time_period'] == sel_time_period]
     
-    print(epsilon_edge.head())    
     return epsilon_edge
 
 def plot_edge_expansion(df_edges, base_data, show_fig, save_fig, filename):
@@ -103,9 +101,63 @@ def process_and_plot_expansion_edge(output, base_data, sel_time_period, sel_scen
     print("Making plot...")
     filename = f"Data/Output/Plots/EdgeExpansion/edge_expansion_plot_{sel_time_period}_{sel_scenario}_{cumulative}.png"
     plot_edge_expansion(df_edges, base_data, show_fig, save_fig, filename)
+    
+def process_and_plot_expansion_node(output, base_data, sel_time_period, sel_scenario="BB", cumulative=False, show_fig=True, save_fig=False):
+    # process data 
+    print("Processing terminal infrasructure investments...")
+    df_nodes = process_nu_nodes(output.nu_node, sel_time_period, sel_scenario, cumulative)
+
+    for mode in ["Sea", "Rail"]:
+        # make plot
+        print("Making plot...")
+        filename = f"Data/Output/Plots/NodeExpansion/node_expansion_plot_{sel_time_period}_{sel_scenario}_{mode}_{cumulative}.png"
+        plot_node_expansion(df_nodes, base_data, mode, show_fig, save_fig, filename)
 
 
+def process_nu_nodes(nu_node, sel_time_period, sel_scenario, cumulative):
+    nu_node = nu_node.copy()
+    
+    nu_node = nu_node[nu_node['scenario']==sel_scenario]
+    
+    if cumulative:
+        nu_node = nu_node[nu_node['time_period']<=sel_time_period]
+        nu_node = nu_node.groupby(['from', 'scenario']).sum().reset_index()
+    else:
+        nu_node = nu_node[nu_node['time_period'] == sel_time_period]
+    
+    return nu_node
 
+def plot_node_expansion(df_nodes, base_data, mode, show_fig, save_fig, filename):
+    
+    df_nodes = df_nodes.copy()
+    df_nodes = df_nodes[df_nodes['mode']==mode]
+    
+    fig, ax, mapp, node_xy_offset, coordinate_mapping, node_x, node_y = plot_base_map_start(base_data)
+    
+    #if the weight is larger than 0, plot the node
+    for index, row in df_nodes.iterrows():
+        node = row['from']
+        weight = row['weight']
+        if weight > 0:
+            x, y = coordinate_mapping[node]
+            ax.scatter(x, y, s=100, c='red', zorder=100)
+            
+    #show and save the figure
+    from matplotlib.lines import Line2D
+    custom_lines = [Line2D([0], [0], color='blue', lw=3),]
+    plt.legend(custom_lines, [f'Expanded Nodes for {mode}'])
+
+    #set size
+    scale = 1.3
+    plot_width = 5 #in inches
+    plot_height = scale * plot_width
+    plt.gcf().set_size_inches(plot_width, plot_height, forward=True) #TODO: FIND THE RIGH TSIZE
+    #save figure
+    if save_fig:
+        plt.savefig(filename, bbox_inches="tight")
+    #show figure
+    if show_fig:
+        plt.show()
 
 
 #####################################################################################
@@ -135,5 +187,5 @@ if True:
     #for t in [2023, 2028, 2034, 2040, 2050]:
     for t in [2023, 2028, 2034, 2040, 2050]:
         process_and_plot_expansion_edge(output, base_data, t, sel_scenario, cumulative=False, show_fig=False, save_fig=True)
-        
-print(base_data.N_NODES)
+        process_and_plot_expansion_node(output, base_data, t, sel_scenario, cumulative=False, show_fig=False, save_fig=True)
+print(output.nu_node.head(100))        
